af3e90c441ac6993a14f96e196b4cdedb5f2dde5
Brian Clapper <bmc@ardentex.com>
2011-08-31 12:30:37 -0400

    Updated API docs

---
+++ api/index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"grizzled" : [], "grizzled.slf4j" : [{"object" : "grizzled\/slf4j\/Logger$.html", "class" : "grizzled\/slf4j\/Logger.html", "name" : "grizzled.slf4j.Logger"}, {"trait" : "grizzled\/slf4j\/Logging.html", "name" : "grizzled.slf4j.Logging"}]};
